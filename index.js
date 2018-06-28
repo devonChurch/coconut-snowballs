9b0693679dd4fb0fd0bb2c26c6c1097263b6e35e
strivegfx <notifydevon@gmail.com>
2018-06-28 21:21:55 +1200

    pull over Translate React component from CodeSandbox

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-console.log("coconut-snowballs index.js");
+import { Component } from "react";
+
+class Translate extends Component {
+  state = {
+    language: "en",
+    translations: null
+  };
+
+  englishOption = { code: "en", name: "English" };
+
+  options = [
+    this.englishOption,
+    { code: "ar", name: "Arabic" },
+    { code: "zh", name: "Chinese" },
+    { code: "fr", name: "French" },
+    { code: "de", name: "German" },
+    { code: "pt", name: "Portuguese" },
+    { code: "es", name: "Spanish" }
+  ];
+
+  createSelectOptions = () => {
+    const {
+      options,
+      props: { languages },
+      englishOption
+    } = this;
+
+    return [
+      englishOption,
+      ...options.reduce(
+        (acc, { code, name }) =>
+          languages.includes(code) ? [...acc, { code, name }] : acc
+      )
+    ];
+  };
+
+  testIsEnglish = language => {
+    const { state, options, englishOption } = this;
+
+    return (language || state.language) === englishOption.code;
+  };
+
+  handleSelectChange = async event => {
+    const language = event.target.value;
+    const translations = this.testIsEnglish(language)
+      ? this.props.english
+      : this.state.translations || JSON.parse(await requestData());
+
+    this.setState({ language, translations });
+  };
+
+  render = () => {
+    const { children, id, languages, english } = this.props;
+    const { language, translations } = this.state;
+    const translation = this.testIsEnglish() ? english : translations[language];
+
+    return (
+      <div>
+        <div>
+          <select value={language} onChange={this.handleSelectChange}>
+            {this.createSelectOptions().map(({ name, code }) => (
+              <option value={code}>{name}</option>
+            ))}
+          </select>
+        </div>
+        {children(translation)}
+      </div>
+    );
+  };
+}
